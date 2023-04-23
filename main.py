b7edc6a37e052e713901992896882497d7f177e2
danielvisser333 <danielvisser10@outlook.com>
2023-04-23 14:14:23 +0200

    Create combined images

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import astropy.io.fits as fits
 import ccdproc
+import astroalign as aa
+import math
 
 
 # Print a 2D array with header
@@ -34,7 +36,7 @@ def inverse_average(data):
 
 def create_masterflat(files, f_out):
     print("Creating masterflat " + f_out)
-    temp_files = ["./processing/"+os.path.basename(filename) for filename in files]
+    temp_files = ["./processing/" + os.path.basename(filename) for filename in files]
     for i in range(len(files)):
         add_pedestal(files[i], temp_files[i])
     masterflat = ccdproc.combine(temp_files, method='average', unit='adu',
@@ -62,9 +64,35 @@ def flatfield_normalisation(filenames_in, filenames_out, masterflat):
         fits.writeto(file_out, dataf, header, overwrite=True)
 
 
+# Compute and save image overlaid onto another one
+def align_image(source, target, output):
+    targetdata = fits.getdata(target)
+    sourcedata = fits.getdata(source)
+    t_data, (source_pos_array, target_pos_array) = aa.find_transform(sourcedata, targetdata)
+    print(f"Image {source}, translation {t_data.translation}, rotation {t_data.rotation}, scale {t_data.scale}")
+    assert (abs(t_data.rotation * 180 / math.pi) < 0.01)  # Ensure the images properly align
+    assert (abs(t_data.scale - 1) < 0.0002)
+    data_translated, footprint = aa.apply_transform(
+        t_data, np.int32(sourcedata), np.int32(targetdata))
+    fits.writeto(output, data_translated, overwrite=True)
+
+
+def combine_images(files_in, file_out):
+    aligned_image_paths = ["./processing/aligned%d.fit" % i for i in range(len(files_in))]
+    for i in range(len(files_in)):  # Align all images
+        align_image(files_in[i], files_in[0], aligned_image_paths[i])
+    combined_image = ccdproc.combine(aligned_image_paths, method="average", unit="adu")
+    print(f"Average of image {file_out}, {np.array(combined_image).mean()}")
+    combined_image.write('./processing/combined_temporary.fit', overwrite=True)
+    data, header = fits.getdata('./processing/combined_temporary.fit', 0, header=True)  # Only take the PRIMARY image
+    fits.writeto(file_out, data, header, overwrite=True)
+    print(f"Created new combined image {file_out}")
+
+
 # Start
 print("Observational Astronomy Data")
 
+# Flat files are the calibration images
 flat_g_file_names = ["./data/flat_g/pic%d.fit" % i for i in range(1, 11)]
 flat_r_file_names = ["./data/flat_r/pic%d.fit" % i for i in range(1, 11)]
 sloan_g_file_names = ["./data/sloan_g/pic%d.fit" % i for i in range(1, 11)]
@@ -98,22 +126,20 @@ table(sloan_r_image_headers, ["NAME", "FILTER", "EXPOSURE", "AVERAGE"])
 # Create the master-flats
 flat_g_master = create_masterflat(flat_g_file_names, './processing/flat_g_master.fit')
 flat_r_master = create_masterflat(flat_r_file_names, './processing/flat_r_master.fit')
-sloan_g_master = create_masterflat(sloan_g_file_names, './processing/sloan_g_master.fit')
-sloan_r_master = create_masterflat(sloan_r_file_names, './processing/sloan_r_master.fit')
 
 print(
-    f"Averages of masterflats: {np.mean(flat_g_master)}, {np.mean(flat_r_master)}, {np.mean(sloan_g_master)}, {np.mean(sloan_r_master)}")
+    f"Averages of masterflats: {np.mean(flat_g_master)}, {np.mean(flat_r_master)}")
 print(
-    f"Standarddeviations of masterflats: {np.std(flat_g_master)}, {np.std(flat_r_master)}, {np.std(sloan_g_master)}, {np.std(sloan_r_master)}")
+    f"Standarddeviations of masterflats: {np.std(flat_g_master)}, {np.std(flat_r_master)}")
 
 # Correct the images (normalise)
 flat_g_file_names_out = ["./processing/picC%d.fit" % i for i in range(1, 11)]
 flat_r_file_names_out = ["./processing/picC%d.fit" % i for i in range(1, 11)]
-sloan_g_file_names_out = ["./processing/picC%d.fit" % i for i in range(1, 11)]
-sloan_r_file_names_out = ["./processing/picC%d.fit" % i for i in range(1, 11)]
 
 flatfield_normalisation(flat_g_file_names, flat_g_file_names_out, './processing/flat_g_master.fit')
 flatfield_normalisation(flat_r_file_names, flat_r_file_names_out, './processing/flat_r_master.fit')
-flatfield_normalisation(sloan_g_file_names, sloan_g_file_names_out, './processing/sloan_g_master.fit')
-flatfield_normalisation(sloan_r_file_names, sloan_r_file_names_out, './processing/sloan_r_master.fit')
 
+# Align and combine the images
+
+combine_images(sloan_g_file_names, "./processing/sloan_g_combined.fit")
+combine_images(sloan_r_file_names, "./processing/sloan_r_combined.fit")
